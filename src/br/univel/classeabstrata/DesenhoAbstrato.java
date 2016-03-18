f860c14ca907c9770ab567a15f3fdeefcffbde12
Mychel Rennó <mychelrenno@gmail.com>
2016-03-18 20:39:44 -0300

    teste

---
+++ src/br/univel/classeabstrata/DesenhoAbstrato.java
@@ -0,0 +1,23 @@
+package br.univel.classeabstrata;
+
+//import br.univel.interfaces.Desenho;
+
+abstract class DesenhoAbstrato{
+
+	public void identificar() {
+		System.out.println("===================================");
+		System.out.println(getNome());
+		System.out.println("===================================");
+		System.out.println("\n");
+	}
+	
+	protected abstract String getNome();
+	
+	public void executar(){
+		identificar();
+		desenhar();
+	}
+	
+	protected abstract void desenhar();
+	
+}
